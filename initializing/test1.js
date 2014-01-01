ccc644f8472709a5db642d1ee546374b46f37652
kwagjj <kwagjj@gmail.com>
2014-01-02 03:49:20 +0900

    Update and rename test9.js to test1.js

--- initializing/test1.js
+++ initializing/test1.js
@@ -52,14 +52,10 @@ function sellwin(){
 function test(){
 	var testurl="https://rawgithub.com/kwagjj/mrmine-macro/master/initializing/sell_window_ver2.html";
 	$.get(testurl,function(data,status){
-		function(function(){
-			alert('inner callback'); } )
-			{
-		var sellwinhtml=data;
-		}
-		
-	
-		
+		sellwinhtml=data;
 	});
+	
+	setTimeout(1000);
+	var win=window.open("","name","width=300,height=300");
 	
 }
