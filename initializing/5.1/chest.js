daecc6f3f68c03256f085b950b740c63f30d1aa8
kwagjj <kwagjj@gmail.com>
2014-01-06 19:46:37 +0900

    1

---
+++ initializing/5.1/chest.js
@@ -0,0 +1,105 @@
+// do it every five minutes?
+
+var chestlog="";
+var chestcount=0;
+var intervalobj;
+var chestlog;
+
+function date_time(){
+		var date = new Date();
+        
+        month = date.getMonth()+1;
+		
+        
+        d = date.getDate();
+        
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ' '+month+'-'+d+' '+h+':'+m+':'+s;
+        return result;
+		
+
+}
+
+
+
+function chestcheck(){
+		document.getElementById("DOWNALLB").onclick(); // press downallb
+		var arr='abcdefghij';
+		var keeploop=1;
+		chestcount=0;
+
+		// loop for checking and clicking chests on the 5th line
+		while(keeploop==1){
+
+
+			for(i=0;i<10;i++)
+			{
+					document.getElementById("L5"+arr[i]).onclick();
+					if(document.getElementById("CHESTD").style.visibility=="visible"){
+						chestcount++;
+					}
+					document.getElementById('OPENIT').onclick();
+					document.getElementById('OPENIT').onclick();
+			} //finished checking the botton line
+
+			document.getElementById("UPB").onclick(); // go one line up
+			
+			if(document.getElementById("HELPBUTTON").style.visibility=="visible"){
+				keeploop=0;
+				var text1=date_time()+" -- "+chestcount+" checked\n";
+				chestlog+=text1;
+			}
+
+		}
+
+		
+
+		// display chestcount somewhere
+		
+}
+
+function startcheck(){
+    intervalobj=setInterval(checkchest,3000);
+}
+
+
+function stopcheck(){
+    clearInterval(intervalobj);
+    
+}
+
+
+shortcut.add("c",function chestlog(){
+        
+        
+        if(chestwin!=undefined && !chestwin.closed){ //when opened window exists,
+        chestwin.close(); // close the opened window
+        chestwin=undefined; // nullify the win variable. give the win variable a fresh start;
+        
+        }
+  
+
+        var testurl="https://rawgithub.com/kwagjj/mrmine-macro/master/initializing/5.1/chestlog.html";
+        $.get(testurl,function(data,status){
+        
+                chestwin=window.open("","chestlog","width=300,height=400");
+                chestwin.document.write(data);
+                chestwin.load();
+        });
+                
+} );
+
---
+++ initializing/5.1/init.js
@@ -0,0 +1,54 @@
+
+
+var sellstatus=0; // if selling=1, not selling=0;
+var sellorder=[1,1,1,1,1,0,0,0,0,0,0,0];  // list of ones to sell. order as written in table.
+
+var loopintveral;
+
+
+function startsell(){
+	loopinterval=setInterval(looping,500);
+	sellstatus=1;
+}
+
+function stopsell(){
+	clearInterval(loopinterval);	
+	sellstatus=0;
+}
+
+
+
+function looping(){
+	
+  //perhaps add a code that opends PLACE1 before clicking the sell buttons.. just in case
+	
+		for(i=0;i<sellorder.length;i++){	
+			if(sellorder[i]==1){
+				var j=i+2;
+				document.getElementById("SB"+j).onclick();
+			
+		}	
+        }
+	
+}
+
+
+
+
+
+
+shortcut.add("s",function sellwin(){
+  
+
+	var testurl="https://rawgithub.com/kwagjj/mrmine-macro/master/initializing/ver4/sell_window_ver4.html";
+	$.get(testurl,function(data,status){
+	
+		var win=window.open("","sell_window","width=300,height=400");
+		win.document.write(data);
+		win.load();
+	});
+		
+} );
+
+
+ 
