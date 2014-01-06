81a7916ecf78b300d140b1c84903a50fdb4bd004
kwagjj <kwagjj@gmail.com>
2014-01-07 03:24:34 +0900

    Update snippetcode.js

--- versions/5.1/snippetcode.js
+++ versions/5.1/snippetcode.js
@@ -6,22 +6,24 @@
   document.head.appendChild(script); 
 
 
+
+
 //inserting shortcut.js plugin
-  var url="https://rawgithub.com/kwagjj/mrmine-macro/take3/basic_js_files/shortcut.js";
+  var url="https://raw.github.com/kwagjj/mrmine-macro/take3/basic_js_files/shortcut.js";
   var script=document.createElement('script');
   
   script.src=url;
   document.head.appendChild(script); 
 
+
   
+
 
 // insert init.js
 var url="https://rawgithub.com/kwagjj/mrmine-macro/take3/versions/5.1/init.js";
+
 
 var script=document.createElement('script');
 script.type='text/javascript';
 script.src=url;
 document.head.appendChild(script);  
-
-
-
